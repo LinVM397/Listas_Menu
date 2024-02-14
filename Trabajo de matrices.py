7e0a683b480be26175d2327ff00867c2e0d93d79
LinVM397 <159825695+LinVM397@users.noreply.github.com>
2024-02-14 11:25:39 -0800

    Add files via upload

---
+++ Trabajo de matrices.py
@@ -0,0 +1,17 @@
+
+matriz=[0,0,0],[0,0,0]
+
+matriz[0][0]=int(input('Dame el elemento [0][0]: '))
+matriz[0][1]=int(input('Dame el elemento [0][1]: '))
+matriz[0][2]=int(input('Dame el elemento [0][2]: '))
+matriz[1][0]=int(input('Dame el elemento [1][0]: '))
+matriz[1][1]=int(input('Dame el elemento [1][1]: '))
+matriz[1][2]=int(input('Dame el elemento [1][2]: '))
+
+print('La matriz creada es: ')
+print('El elemento [0][0] es:',matriz[0][0])
+print('El elemento [0][1] es:',matriz[0][1])
+print('El elemento [0][2] es:',matriz[0][2])
+print('El elemento [1][0] es:',matriz[1][0])
+print('El elemento [1][1] es:',matriz[1][1])
+print('El elemento [1][2] es:',matriz[1][2])
